f8f327e3537d52d520aaa5f43338953896327722
Alessandro <alessandro.mancinelli89@gmail.com>
2020-10-20 17:27:03 +0200

    Final version
    added toFixed

--- js/script.js
+++ js/script.js
@@ -1,13 +1,14 @@
 var tratta = parseInt(prompt('Quanti chilometri verranno percorsi?'));
 var anni = parseInt(prompt('Quanti anni ha il passeggero?'));
 
-tratta = tratta * 0.21;
-
+var prezzo = tratta * 0.21;
+var prezzoMinorenni = prezzo * 20/100;
+var prezzoSenior = prezzo * 40/100;
 
 if (anni < 18){
-  document.getElementById('prezzo').innerHTML= tratta - ( tratta * 20/100) + '€';
-} else if (anni == 65 || anni > 65){
-  document.getElementById('prezzo').innerHTML= tratta - ( tratta * 40/100) + '€';
+  document.getElementById('prezzo').innerHTML= prezzoMinorenni.toFixed(2) + '€' ;
+} else if (anni >= 65){
+  document.getElementById('prezzo').innerHTML= prezzoSenior.toFixed(2) + '€' ;
 } else{
-  document.getElementById('prezzo').innerHTML= tratta + '€';
+  document.getElementById('prezzo').innerHTML= prezzo.toFixed(2) + '€' ;
 }
