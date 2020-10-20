376063510f1880a3ad44cf8f370d60895ab57dc0
Alessandro <alessandro.mancinelli89@gmail.com>
2020-10-20 16:09:44 +0200

    Final v2
    2 variables

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
-var km = prompt('Quanti chilometri verranno percorsi?');
+var tratta = prompt('Quanti chilometri verranno percorsi?');
 var anni = prompt('Quanti anni ha il passeggero?');
 
-var prezzotratta = km * 0.21;
+tratta = tratta * 0.21;
 
 
 if (anni < 18){
-  document.getElementById('prezzo').innerHTML= prezzotratta - ( prezzotratta * 20/100) + '€';
+  document.getElementById('prezzo').innerHTML= tratta - ( tratta * 20/100) + '€';
 } else if (anni == 65 || anni > 65){
-  document.getElementById('prezzo').innerHTML= prezzotratta - ( prezzotratta * 40/100) + '€';
+  document.getElementById('prezzo').innerHTML= tratta - ( tratta * 40/100) + '€';
 } else{
-  document.getElementById('prezzo').innerHTML= prezzotratta + '€';
+  document.getElementById('prezzo').innerHTML= tratta + '€';
 }
